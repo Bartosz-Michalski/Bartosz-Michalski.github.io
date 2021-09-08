06cca0e9c8b81feeff59af0d3cfacefbb2f702a8
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-09-08 13:13:37 +0200

    add new functionality, top bar hide when scroll down and appear when scroll up

---
+++ components/menu/navigation.js
@@ -0,0 +1,34 @@
+const navLinks = document.querySelectorAll(".navigation__link");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    const target = document.querySelector(`.main__section[name="${link.name}`).offsetTop;
+
+    window.scrollTo({ top: target, behavior: "smooth" });
+  });
+});
+
+// TOP BAR STICKY
+
+let lastScroll = 0;
+
+const topBarSticky = () => {
+  const currentScroll = window.pageYOffset;
+  const topBar = document.querySelector(".wrapper-top");
+
+  if (!topBar.classList.contains("wrapper-top--active")) {
+    if (currentScroll <= 0) {
+      document.body.classList.remove("scroll-up");
+    } else if (currentScroll > lastScroll && !document.body.classList.contains("scroll-down")) {
+      document.body.classList.remove("scroll-up");
+      document.body.classList.add("scroll-down");
+    } else if (currentScroll < lastScroll && document.body.classList.contains("scroll-down")) {
+      document.body.classList.remove("scroll-down");
+      document.body.classList.add("scroll-up");
+    }
+
+    lastScroll = currentScroll;
+  }
+};
+
+window.addEventListener("scroll", topBarSticky);
