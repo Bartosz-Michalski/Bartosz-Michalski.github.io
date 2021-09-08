f5cc73271e5caa437663b20335e107e358271f92
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-09-08 19:14:02 +0200

    supplement some data of slides

--- components/carousel/slide-list.js
+++ components/carousel/slide-list.js
@@ -1,23 +1,23 @@
 const slideList = [
   {
-    title: "Zaadoptuj Programiste App",
-    text: "Text 1",
-    img: "",
+    title: `"Zaadoptuj Programiste" App`,
+    text: "This project is being created by me and my two friends, using ReactJS. It is a web application which allows to register a new user. Each user can create and publish their own projects, describe them in detail and specify team members they are looking for or search for existing project created by someone else and apply.",
+    img: "../../img/mockup-zaadoptuj-programiste.png",
     address: "https://infoshareacademy.github.io/jfddr1-maklowiczeprogramowania-app/",
     sourceCode: "https://github.com/infoshareacademy/jfddr1-maklowiczeprogramowania-app",
   },
   {
-    title: "Email Client Layout",
-    text: "Text 2",
-    img: "",
+    title: `"Ygrasilmail Email Client" Layout`,
+    text: "This is my second layout project, which was created as a homework in a bootcamp for Front-End Developers, run by infoShare Academy. It is a Viking style layout of an email client, fully designed and coded by me, using tools such as flexbox and grid.",
+    img: "../../img/mockup-ygdrasilmail.png",
     address: "https://bartosz-michalski.github.io/layouts-projects/src/klient-poczty/",
     sourceCode: "https://github.com/Bartosz-Michalski/layouts-projects/tree/master/src/klient-poczty",
   },
   {
-    title: "Memory Game App",
+    title: "To Do App",
     text: "Text 3",
     img: "",
-    address: "https://bartosz-michalski.github.io/jfddr1-project-2/src/memory-game/",
-    sourceCode: "https://github.com/Bartosz-Michalski/jfddr1-project-2/tree/master/src/memory-game",
+    address: "https://bartosz-michalski.github.io/sm-project12-to-do-app/exercise4/index.html",
+    sourceCode: "https://github.com/Bartosz-Michalski/sm-project12-to-do-app",
   },
 ];
